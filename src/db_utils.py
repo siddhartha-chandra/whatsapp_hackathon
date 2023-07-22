f5cf940e23a1af77adb6aae71eafad1f34120f9a
Siddhartha Chandra <siddhartha.chandra0112@gmail.com>
2023-07-22 23:19:34 +0530

    feat: add functions for interacting with database tables

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,7 +1,16 @@
 from src import db
 from datetime import datetime
 from src.food_inventory.models import FoodInventory
+from src.user_defaults.models import UserDefaults
+from src.conversations.models import Conversations
+from sqlalchemy.orm.session import make_transient
 
+def columns(table_obj):
+    return table_obj.columns
+
+### Food Inventory ###
+def get_food_inventory_columns():
+    return columns(FoodInventory)
 
 def fetch_food_inventory_categories():
     return db.session.query(FoodInventory.category).distinct().all()
@@ -46,4 +55,78 @@ def add_to_food_inventory(phone_id, init=False):
         item = FoodInventory(**item)
         db.session.add(item)
 
+    db.session.commit()
+
+
+### User Defaults ###
+def get_user_defaults_columns():
+    return columns(UserDefaults)
+
+def add_to_user_defaults(phone_id):
+    # drop all items in the table 'user_defaults'
+    db.session.query(UserDefaults).delete()
+
+    phone_id = phone_id
+    created_on = datetime.now()
+    updated_on = datetime.now()
+    location = "Andheri(W), Mumbai, India"
+    diet_preferences = ["oil-free", "Vegan", "salt-free", "sugar free"]
+    diet_restrictions = ["papaya", "peanuts"]
+    cooking_appliances = ["toaster", "slow juicer", "blender"]
+    utensils = ["frying pan", "wok", "medium cooking pot"]
+
+    user_defaults = UserDefaults(
+        phone_id=phone_id, 
+        created_on=created_on, 
+        updated_on=updated_on,
+        location=location, 
+        diet_preferences=diet_preferences, 
+        diet_restrictions=diet_restrictions,
+        cooking_appliances=cooking_appliances,
+        utensils=utensils)
+    
+    db.session.add(user_defaults)
+
+    db.session.commit()
+
+def fetch_user_defaults(phone_id):
+    return db.session.query(UserDefaults).filter_by(phone_id=phone_id).first()
+
+def fetch_conversation(phone_id):
+    return db.session.query(Conversations).filter_by(phone_id=phone_id).first()
+
+
+### User Conversations ###
+def get_conversation_columns():
+    return columns(Conversations)
+
+def clear_conversation(phone_id):
+    db.session.query(Conversations).filter_by(phone_id=phone_id).delete()
+    db.session.commit()
+
+def update_user_conversation(phone_id, messages):
+    conversation = fetch_conversation(phone_id)
+    conversation.updated_on = datetime.now()
+    conversation.messages = messages
+
+    # overwrite data in table
+    clear_conversation(phone_id)  
+    make_transient(conversation)  
+    db.session.add(conversation)
+    db.session.commit()
+
+def add_msg_to_conversation(phone_id, messages):
+    # first fetch conversation
+    conversation = fetch_conversation(phone_id)
+    created_on = datetime.now()
+    updated_on = datetime.now()
+    if conversation:
+        conversation.updated_on = updated_on
+        conversation.messages = conversation.messages + messages
+        clear_conversation(phone_id)
+        make_transient(conversation)
+    else:
+        conversation = Conversations(phone_id=phone_id, created_on=created_on, updated_on=updated_on, messages=messages)
+
+    db.session.add(conversation)
     db.session.commit()
