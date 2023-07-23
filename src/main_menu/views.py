4a2ef95b5d66bff555d59f2e2f7654365e656cbf
Siddhartha Chandra <siddhartha.chandra0112@gmail.com>
2023-07-23 10:24:28 +0530

    feat: finalize code in the view for main_menu

--- src/main_menu/views.py
+++ src/main_menu/views.py
@@ -1,7 +1,8 @@
 import json
-from src.utils import get_buttons_from_data, display_main_menu, display_food_inventory
+from src.utils import get_buttons_from_data, display_main_menu, display_food_inventory, display_user_preferences
 from src.db_utils import is_food_inventory_empty, add_to_food_inventory
 from src.recommend.views import handle_request as handle_recommend_request
+from src.user_defaults.views import handle_request as handle_userdefaults_request
 
 
 
@@ -12,7 +13,7 @@ def handle_request(r, json_data, logging):
         reply_id = json_data["data"]["body"]["id"]
 
         # VIEW ITEMS
-        if reply_id == "view":
+        if reply_id == "view_update":
             phone_id = json_data["caller"]["id"]
             no_data_present = is_food_inventory_empty(phone_id)
             if no_data_present:
@@ -20,21 +21,14 @@ def handle_request(r, json_data, logging):
                 r.send_text("Sample stock added in Food inventory")
 
             display_food_inventory(r)
-        # UPDATE ITEMS
-        elif reply_id == "update":
-            buttons = get_buttons_from_data(
-                title="Food inventory update",
-                button_names=["Update by category", "Main Menu"]
-            )
-            r.set_bot_state("Food_Update_Menu")
-
+        
         # MEAL RECOMMEND
         elif reply_id == "recommend":
             handle_recommend_request(r, json_data, logging)
 
         # USER PREFERENCES
         elif reply_id == "preferences":
-            pass
+            display_user_preferences(r, json_data["caller"]["id"])
 
         else:
             r.send_text("Invalid Request data")
